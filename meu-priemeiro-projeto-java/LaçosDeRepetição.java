37b0ee73b2a5e35441ce10e95a9a48ffbc2ac132
Guilherme Vieira <161070870+DevGuivieiraa@users.noreply.github.com>
2025-02-05 16:40:22 -0300

    Create LaçosDeRepetição.java

---
+++ meu-priemeiro-projeto-java/LaçosDeRepetição.java
@@ -0,0 +1,28 @@
+public class LaçosDeRepetição {
+    //for
+    // public static void main(String[] args) {
+    //     for(int i = 1; i<= 5; i++){
+    //         System.out.println("Número: " + i);
+    //     }
+    // }
+
+    //WHILE
+    // public static void main(String[] args) {
+    //     int contador = 1;
+
+    //     while (contador <= 5){
+    //         System.out.println("Contagem: " + contador);
+    //         contador++;
+    //     }
+    // }
+
+    //DO WHILE
+    public static void main(String[] args) {
+        int num = 1;
+
+        do { 
+            System.out.println("Número: " + num);
+            num++;
+        } while (num <= 5);
+    }
+}
