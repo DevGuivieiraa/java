9e9d1331d5055ad1ac491433616993d4c5d16eb9
Guilherme Vieira <161070870+DevGuivieiraa@users.noreply.github.com>
2025-02-05 16:41:48 -0300

    Create EstruturasDeControle.java

---
+++ meu-priemeiro-projeto-java/EstruturasDeControle.java
@@ -0,0 +1,13 @@
+public class EstruturasDeControle {
+    public static void main(String[] args) {
+        int nota = 5;
+
+        if (nota >= 7){
+            System.out.println("Aprovado");
+        }else if (nota >= 3){
+            System.out.println("Em exame");
+        }else{
+            System.out.println("Reprovado");
+        }
+    }
+}
